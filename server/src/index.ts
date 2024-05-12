a9d1259602ffadd2f98691dd181c7518603302cb
Arijeet10 <arijeetsocial@gmail.com>
2024-05-12 21:25:33 +0530

    added cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,6 +10,13 @@ import noteRoutes from "./routes/noteRoutes";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT,PATCH, POST");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 //Middlewares
 app.use(
   cors({

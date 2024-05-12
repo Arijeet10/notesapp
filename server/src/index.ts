63e01dd33f181dde7ec920c51955d901bc6c2830
Arijeet10 <arijeetsocial@gmail.com>
2024-05-12 20:27:38 +0530

    added cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,7 +11,7 @@ const app=express();
 const PORT=process.env.PORT || 3000;
 
 //Middlewares
-app.use(cors({credentials:true,origin:true,methods:["POST","GET","PATCH","DELETE"]}));
+app.use(cors({credentials:true,origin:process.env.BASE_URL,methods:["POST","GET","PATCH","DELETE"]}));
 app.use(compression());
 app.use(cookieParser());
 app.use(bodyParser.json());
